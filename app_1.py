4625329c916b11e144b486af381da275cdb053c0
Ahmed Abdelhafez <ahmedhafez.t.7@gmail.com>
2021-09-16 04:21:42 +0200

    Program that sums two numbers

---
+++ app_1.py
@@ -0,0 +1,8 @@
+def add_two_numbers(num1, num2):
+    total = num1 + num2
+    print("The Sum of you numbers is:", total)
+
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
+
+add_two_numbers(int(num1), int(num2))
